f11c8419a88ba2252a62eb5de26ac5d28e02ef60
Ben Cunningham <benjamin-cunningham@uiowa.edu>
2015-01-10 15:40:02 -0600

    Prevent importing data frame variables as factors
    Amending the import_scrobbles() function to read .csv files as
    characters rather than factors makes the returned data frames more
    robust for plotting and other downstream uses.
    
    The addition of a data.fmt parameter allows for more control over date
    formatting in the returned data frame.

--- R/import_scrobbles.R
+++ R/import_scrobbles.R
@@ -1,19 +1,20 @@
 #' Import a Last.fm scrobbles sheet as a data frame.
 #'
 #' This function initializes a data frame of Last.fm scrobbles.
-#' @param file The name of the file which the scrobble data is to be read from.z
+#' @param file The name of the file which the scrobble data is to be read from.
 #' @param header A logical value indicating whether the file contains the names of the variables as its first line.
+#' @param date.fmt The date format that should be used in the returned data frame.
 #' @return A data frame with columns Artist, Album, Song, and Date.
-#' @examples \dontrun{
-#' import_scrobbles(file = "scrobbles.csv")
-#' }
+#' @examples import_scrobbles("scrobbles.csv")
 
 import_scrobbles <- function(file,
-                             header = FALSE){
+                             header = FALSE,
+                             date.fmt = "%d %b %Y %H:%M") {
   
   scrobbles <- read.csv(
     file,
-    header = header
+    header = header,
+    stringsAsFactors = FALSE
   )
   
   nms <- c(
@@ -24,6 +25,11 @@ import_scrobbles <- function(file,
   )
   
   names(scrobbles) <- nms
+  
+  scrobbles$Date <- strptime(
+    scrobbles$Date,
+    format = date.fmt
+  )
   
   scrobbles
   
