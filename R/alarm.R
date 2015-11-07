4824b02897bc658e30aa76aa4a8d9a365104cb5d
Ben Cunningham <benjamin-cunningham@uiowa.edu>
2015-11-06 18:01:30 -0600

    Convert paths to MS-DOS format
    Passed paths containing spaces were causing problems with the alarm
    function and its interaction with Windows shells.

--- R/alarm.R
+++ R/alarm.R
@@ -1,27 +1,25 @@
 #' Play an alarm
 #'
 #' Plays a sound file using Powershell.
-#' @param x Location of the sound file.
+#' @param x Location of the sound file relative the path specified in
+#'   \code{ben.options('path.alarms')}
 #' @export
 alarm <- function(x) {
 
-  cmd <- cat(
-    'start powershell -windowstyle hidden -c ',
-    '(New-Object Media.SoundPlayer "',
-    ben.options('path.alarms'),
-    x,
-    '").PlaySync();',
-    sep = ''
+  path <-
+    ben.options('path.alarms') %>%
+    path.expand() %>%
+    paste0(x) %>%
+    normalizePath() %>%
+    paste0('cmd /c for %A in ("', ., '") do @echo %~sA') %>%
+    shell(intern = TRUE)
+
+  cmd <- paste0(
+    'start powershell -windowstyle hidden -c (New-Object Media.SoundPlayer "',
+    path,
+    '").PlaySync();'
   )
 
   shell(cmd)
 
 }
-
-paste0(
-  'start powershell -windowstyle hidden -c (New-Object Media.SoundPlayer "',
-
-  '").PlaySync();'
-)
-
-paste0(1,2)
