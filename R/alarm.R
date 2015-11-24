c1adf935f7ddbfb6c0dc0e39fa53bcd01fa470aa
Ben Cunningham <benjamin-cunningham@uiowa.edu>
2015-11-24 16:51:25 -0600

    Merge new Rprofile options design into functions

--- R/alarm.R
+++ R/alarm.R
@@ -1,15 +1,17 @@
 #' Play an alarm
 #'
 #' Plays a sound file using Powershell.
-#' @param x Location of the sound file relative the path specified in
-#'   \code{ben.options('path.alarms')}
+#' @param x Name of the sound file relative the path specified in
+#'   \code{options('ben.alarms')}
 #' @export
+#' @examples
+#' alarm('Sound.wav')
 alarm <- function(x) {
 
   path <-
-    ben.options('path.alarms') %>%
+    getOption('ben.alarms') %>%
     path.expand() %>%
-    paste0(x) %>%
+    paste0(., x) %>%
     normalizePath() %>%
     paste0('cmd /c for %A in ("', ., '") do @echo %~sA') %>%
     shell(intern = TRUE)
