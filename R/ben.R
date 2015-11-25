dac36300d848fe350735a000631b9a6ef306f21a
Ben Cunningham <benjamin-cunningham@uiowa.edu>
2015-11-24 19:35:52 -0600

    Add doc for package

---
+++ R/ben.R
@@ -0,0 +1,19 @@
+#' ben
+#'
+#' Personal R package of Ben Cunningham.
+#'
+#' @section Package Options:
+#'
+#' The following options should be set in \code{.Rprofile} to customize
+#' the behavior of some functions.
+#'
+#' \itemize{
+#'
+#'  \item \code{ben.alarms}: Directory of sound assets; for example
+#'    \code{~/R/alarms/}
+#'
+#' }
+#'
+#' @docType package
+#' @name ben
+NULL
